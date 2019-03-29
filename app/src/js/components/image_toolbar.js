f037589a975185ffbd18e780a3974209168c0d32
Bohan Zhai <zhaibohan@berkeley.edu>
2019-03-29 16:17:20 -0700

    add remove button

--- app/src/js/components/image_toolbar.js
+++ app/src/js/components/image_toolbar.js
@@ -3,12 +3,37 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import {goCreate} from '../common/service';
-import CreateIcon from '@material-ui/core/SvgIcon/SvgIcon';
+import CreateIcon from '@material-ui/icons/Create';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles/index';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 // player-controls
 // Get the task
 //
 /* Sidebar: mainList */
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    },
+});
+
+function IconLabelButtons(props) {
+    const { classes } = props;
+    return (
+        <div>
+            <Button variant="contained" className={classes.button}>
+                Remove
+            <DeleteIcon className={classes.rightIcon} />
+            </Button>
+        </div>
+    );
+}
+const DeletButton = withStyles(styles)(IconLabelButtons);
 
 type Props = {
     categories: Object,
@@ -27,6 +52,7 @@ export class ToolBar extends React.Component<Props> {
                     </ListItemIcon>
                     <ListItemText primary='Create new project' />
                 </ListItem>
+                <DeletButton />
             </div>
         )
     }
